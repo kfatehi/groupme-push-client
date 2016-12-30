349acd0e9fcc719919e1c6f8827f01f2b026f5e2
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-12-30 08:24:46 -0800

    add example in there, make it consumable

---
+++ README.md
@@ -0,0 +1,7 @@
+# groupme-push-client
+
+This client implements a portion of GroupMe's Bayeux/Faye/WebSocket push protocol.
+
+It also implements a few of the REST API calls.
+
+Made for https://github.com/kfatehi/matrix-appservice-groupme
---
+++ example.js
@@ -0,0 +1,34 @@
+const { ACCESS_TOKEN, USER_ID } = require('./config');
+
+if (!ACCESS_TOKEN || !USER_ID) {
+  console.error('create config.js per the sample file');
+  process.exit(1);
+}
+
+const Client = require('./')
+const client = new Client({ACCESS_TOKEN});
+
+client.connect().then((client) => {
+  return client.subscribe(`/user/${USER_ID}`).then(userSub => {
+    console.log('subscribed to user messages');
+    userSub.on('line.create', function(data) {
+      console.log('line created', data.alert);
+    });
+  })
+}).then(()=>{
+  return client.api.getGroups()
+}).then(({data:{response}}) => {
+  let groups = response;
+  return groups[0];
+}).then(group => {
+  return client
+    .subscribe(`/group/${group.id}`)
+    .then(groupSub=>{
+      console.log('subscribed to events regarding group', group.name);
+      groupSub.on('typing', data=>{
+        console.log('typing happening in group', group.name);
+      })
+    })
+}).catch(err=>{
+  console.error(err);
+})
